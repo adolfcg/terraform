04db37754ee987768a60ff9d4080b33ac3dd75ff
Adolfo <adolph.cg@gmail.com>
2025-09-21 20:19:41 -0600

    adding some lifecycle

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
 resource "aws_s3_bucket" "this" {
   bucket = "this-is-my-bucket-name-12345" # Change to a unique bucket name
+  lifecycle {
+    prevent_destroy = true   # Terraform will never try to delete this bucket
+  }
 }
